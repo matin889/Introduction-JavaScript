80e37b8a2b3ea29e5e592012b6615d451efa3cde
Abdul Matin <matin@MATINs-MacBook-Air.local>
2022-10-19 09:37:00 +0200

    JavaScript file added and updated

--- js--splitta-notan-master/js/index.js
+++ js--splitta-notan-master/js/index.js
@@ -1,1 +1,16 @@
+        const summan = document.getElementById("sum");
+        const friends = document.getElementById("numberOfFriends");
+        const tips = document.getElementById("tip");
+        const button = document.querySelector(".form__button");
+        const section = document.getElementById("showSum");
 
+        button.addEventListener("click", calculate);
+
+
+function calculate() {
+        const totalAmount = parseInt(summan.value) + (parseInt(tips.value) / 100) * parseInt(summan.value);
+        const individualAmount = totalAmount / parseInt(friends.value);
+        document.getElementById("result").innerHTML = individualAmount;
+        section.classList.toggle("hide");
+    }
+    
